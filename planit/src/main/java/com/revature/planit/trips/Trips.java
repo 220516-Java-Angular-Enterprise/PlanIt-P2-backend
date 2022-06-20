c7e3fe323b7d20fcbadb30884e1a069496468de6
Hal Coronado <hallie_coronado1@baylor.edu>
2022-06-20 16:15:26 -0500

    made services repos and a generic new trip request

--- planit/src/main/java/com/revature/planit/trips/Trips.java
+++ planit/src/main/java/com/revature/planit/trips/Trips.java
@@ -1,4 +1,12 @@
 package com.revature.planit.trips;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "trip")
 public class Trips {
+    public Trips(String hotel, String night_activity, String day_activity, String food) {
+    }
 }
